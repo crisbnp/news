c630257a69f1a6b2ccf87169068e0a07779a8ad5
Cristien Natal <crisbnp@icloud.com>
2019-03-20 01:47:51 +0000

    just deleted a block of commented out code  used to check syntax fromw weather app

--- public/index.js
+++ public/index.js
@@ -29,17 +29,3 @@ document.getElementById("submit").addEventListener("click", () => {
         })
     }
 )
-
-// document.getElementById("submit").addEventListener("click", () => {
-//     fetch("http://localhost:3000/weather?address=" + input.value)
-//         .then((response) => {
-//             console.log(response)
-//             return response.json()
-//         })
-//         .then((data) => {
-//             console.log(data)
-//             weather.innerHTML = data.forecast
-//             place.innerHTML = data.location
-//         })
-//     }
-// )
